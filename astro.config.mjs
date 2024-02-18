5bf8ae897ba0be8a76263a6a03d527c7d8487ecb
AntoineDujar <antoine.v.dujardin@gmail.com>
2024-02-17 22:57:03 -0500

    init

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,9 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    i18n: {
+        defaultLocale: "fr",
+        locales: ["fr", "en"]
+      }
+});
