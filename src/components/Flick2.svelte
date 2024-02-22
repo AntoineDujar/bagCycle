38d73e8ebd622e0c537dfdd301945f318a3f8a2f
AntoineDujar <antoine.v.dujardin@gmail.com>
2024-02-22 13:53:32 -0500

    salon carousel

---
+++ src/components/Flick2.svelte
@@ -0,0 +1,49 @@
+<script lang="ts">
+  import Flicking, { FlickingPanel } from "@egjs/svelte-flicking";
+  import { AutoPlay, Arrow } from "@egjs/flicking-plugins";
+
+  import "@egjs/flicking-plugins/dist/arrow.css";
+  import "@egjs/flicking-plugins/dist/pagination.css";
+  import "@egjs/svelte-flicking/dist/flicking.css";
+
+  let flicking: Flicking;
+
+  const plugins = [
+    new AutoPlay({ duration: 4000, direction: "NEXT", stopOnHover: true }),
+    new Arrow(),
+  ];
+</script>
+
+<div class="container">
+  <Flicking
+    {plugins}
+    options={{ circular: true, duration: 450, preventDefaultOnDrag: true }}
+    bind:this={flicking}
+  >
+    <FlickingPanel><slot name="image-1" /></FlickingPanel>
+    <FlickingPanel><slot name="image-2" /></FlickingPanel>
+    <FlickingPanel><slot name="image-3" /></FlickingPanel>
+    <svelte:fragment slot="viewport">
+      <span class="flicking-arrow-prev"></span>
+      <span class="flicking-arrow-next"></span>
+    </svelte:fragment>
+  </Flicking>
+</div>
+
+<style>
+  .flicking-arrow-prev::before,
+  .flicking-arrow-prev::after,
+  .flicking-arrow-next::before,
+  .flicking-arrow-next::after {
+    background-color: rgb(255, 255, 255);
+  }
+
+  .container {
+    max-width: 30rem;
+    border-radius: 8px;
+    /* background-color: aqua; */
+    overflow: hidden;
+    height: 40rem;
+    margin-bottom: 0rem;
+  }
+</style>
